06ad6b6d5fb0f6ac21e921dcabe4be51e1355d53
HimadriPaul24 <himadripaul821@gmail.com>
2021-09-02 19:41:38 +0600

    another commit

--- js/app.js
+++ js/app.js
@@ -1,43 +1,69 @@
-// console.log(9);
+const result = document.getElementById('result');
+const searchResult = document.getElementById('search-result');
+
+
+
 const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    console.log(searchText);
+    // console.log(searchText);
 
+    //clear data
     searchField.value = '';
 
 
-    // const url = `https://openlibrary.org/search.json?q=${searchText}`;
 
-    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    searchResult.textContent = '';
+    if (searchText === '') {
+        result.innerText = 'Please Provide A Name';
 
-    // console.log(url);
+    }
+    else {
+        //load data
 
-    fetch(url)
-        .then(res => res.json())
-        // .then(data => console.log(data.docs))
-        .then(data => displaySearchResult(data.docs))
+        const url = `https://openlibrary.org/search.json?q=${searchText}`;
 
+        fetch(url)
+            .then(res => res.json())
+            // .then(data => console.log(data.docs))
+            .then(data => displaySearchResult(data.docs));
+
+
+    }
 }
+
 
 //showing search result 
 const displaySearchResult = docs => {
-    const searchResult = document.getElementById('search-result');
-    docs.forEach(book => {
-        console.log(book);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
+    docs = docs.filter(element => element.author_name !== undefined || element.publisher !== undefined || element.first_publish_year !== undefined || element.cover_i !== undefined);
+    searchResult.textContent = '';
+
+
+    if (docs.length === 0) {
+        result.innerText = 'No Result Found';
+    }
+    else {
+        result.innerText = `Total Result: ${docs.length}`;
+
+        docs.forEach(docs => {
+            console.log(docs);
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
         <div class="card h-100">
-             <img src="..." class="card-img-top" alt="...">
+             <img src="https://covers.openlibrary.org/b/id/${docs.cover_i}-M.jpg" class="card-img-top" alt="book-image">
             <div class="card-body">
-               <h4 class="card-title">${title}</h4>
-               <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.</p>
+               <h3 class="book-title">Book-Title: ${docs.title}</h3>
+               <h4 class="writer-name">Author-name: ${docs.author_name}</h4>
+               <h5 class="publisher">Publisher: ${docs.publisher}</h5>    
+               <h5 class="publish-date">Publish-Date: ${docs.first_publish_year}</h5>    
             </div>
         </div>
         `
-        searchResult.appendChild(div);
-    })
+
+
+            searchResult.appendChild(div);
+        })
+    }
 }
 
