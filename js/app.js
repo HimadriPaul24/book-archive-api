8f614ccfe8a4101fc3e478332f38c88b673c2b80
HimadriPaul24 <himadripaul821@gmail.com>
2021-09-02 14:10:50 +0600

    card added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
-console.log(9);
+// console.log(9);
+const searchBook = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+
+    searchField.value = '';
+
+
+    // const url = `https://openlibrary.org/search.json?q=${searchText}`;
+
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+
+    // console.log(url);
+
+    fetch(url)
+        .then(res => res.json())
+        // .then(data => console.log(data.docs))
+        .then(data => displaySearchResult(data.docs))
+
+}
+
+//showing search result 
+const displaySearchResult = docs => {
+    const searchResult = document.getElementById('search-result');
+    docs.forEach(book => {
+        console.log(book);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+             <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+               <h4 class="card-title">${title}</h4>
+               <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        `
+        searchResult.appendChild(div);
+    })
+}
+
